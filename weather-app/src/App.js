6e1537df913c675ffe081b3fd97f801c2499bdb0
DusanRajcevic1989 <dule.rajca@gmail.com>
2023-03-22 15:55:03 +0100

    add fetch and display weather

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -4,22 +4,38 @@ import Form from "./Components/Form/form";
 import List from "./Components/List /list";
 import useLocalStorageState from "use-local-storage-state";
 import { uid } from "uid";
+import { useEffect } from "react";
 
 function App() {
   const [activities, setActivity] = useLocalStorageState("activiteis", {
     defaultValue: [],
   });
+  const [weather, setWeather] = useLocalStorageState("weather", {
+    defaultValue: [],
+  });
 
-   const isGoodWeather = true;
+  useEffect(() => {
+    fetchWeather();
+
+    return () => {};
+  }, []);
+
+  async function fetchWeather() {
+    const response = await fetch("https://example-apis.vercel.app/api/weather");
+    const data = await response.json();
+    console.log(data);
+    setWeather(data);
+  }
 
   function handleAddActivity(newActivity) {
     setActivity([...activities, { ...newActivity, id: uid() }]);
   }
 
-  console.log(activities);
-
+  function handleDeleteActivity(id) {
+    setActivity(activities.filter((activity) => activity.id !== id));
+  }
   const goodWeatherActivities = activities.filter(
-    (activity) => activity.isForGoodWeather 
+    (activity) => activity.isForGoodWeather
   );
   const badWeatherActivities = activities.filter(
     (activity) => !activity.isForGoodWeather
@@ -28,8 +44,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <List activities={goodWeatherActivities} headline={"go out"} />
-        <List activities={badWeatherActivities} headline={"stay home"} />
+        <h2 className="API-heading">
+          <p className="emoji">{weather.condition}</p>
+          <p className="temp">{weather.temperature}</p>
+        </h2>
+        <List
+          activities={
+            weather.isGoodWeather ? goodWeatherActivities : badWeatherActivities
+          }
+          headline={weather.isGoodWeather ? "go out" : "stay home"}
+          onDeleteActivity={handleDeleteActivity}
+        />
+
         <Form onAddActivity={handleAddActivity} />
       </header>
     </div>
--- weather-app/src/Components/List /list.jsx
+++ weather-app/src/Components/List /list.jsx
@@ -1,21 +1,13 @@
-
-
-export default function List({activities, onDeleteActivity, headline} ) {
-
-  
+export default function List({ headline, activities, onDeleteActivity }) {
   return (
     <div>
       <h2>{headline}</h2>
-      
       {(activities ?? []).map((activity) => (
-        <li key={activity.id}>{activity.activityName}
-          <button
-          onClick={()=> onDeleteActivity?.(activity)}>x</button>
-        </li>)
-      
-      )}
-     
-      
+        <li key={activity.id}>
+          {activity.activityName}
+          <button onClick={() => onDeleteActivity(activity.id)}>X</button>
+        </li>
+      ))}
     </div>
   );
 }
